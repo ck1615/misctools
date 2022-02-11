3e24de243bfa160c90872172fe354494ce7c0b92
Christopher Keegan <ck1615@ic.ac.uk>
2022-02-11 17:56:26 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Module: Miscellaneous tools (misc-tools)
+
+This module contains a set of miscellaneous tools for pre-processing input and
+output files of electronic structure calculations.
+
+Author: Christopher Keegan
+Date: 25/08/2020
+
+Functions taken and adapted from Chris. J. Ablitt: strindex, strindices
+"""
